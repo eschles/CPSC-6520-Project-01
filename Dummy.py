8672078a6a78728b64aed291018ad187c9d4180a
cmurmu <chandni2104@gmail.com>
2023-02-13 23:54:02 -0500

    Added comment for pygame.init() to test commit working from my end.

--- Dummy.py
+++ Dummy.py
@@ -6,6 +6,7 @@ from sys import exit
 # Define Screen Size
 SCREEN_SIZE = SCREEN_WIDTH, SCREEN_HEIGHT = 800, 400
 
+# Initializing the game
 pygame.init()
 screen = pygame.display.set_mode(SCREEN_SIZE)
 
