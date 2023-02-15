be997c5ce9311702ab11d20c7b090a7cd36e9e49
cmurmu <chandni2104@gmail.com>
2023-02-15 11:25:22 -0500

    Added paddle to the left vertical side of the screen in the Dummy.py file

--- Dummy.py
+++ Dummy.py
@@ -6,11 +6,24 @@ from sys import exit
 # Define Screen Size
 SCREEN_SIZE = SCREEN_WIDTH, SCREEN_HEIGHT = 800, 400
 
+# Set the size and position of the paddle
+paddle_width = 16
+paddle_height = 100
+paddle_x = 0
+paddle_y = (SCREEN_HEIGHT - paddle_height) / 2
+
 # Initializing the game
 pygame.init()
 
 # pygame module to initialize a window or screen for display
 screen = pygame.display.set_mode(SCREEN_SIZE)
+
+# Create the paddle surface
+paddle_surface = pygame.Surface((paddle_width, paddle_height))
+paddle_surface.fill((255, 255, 255))  # Set the color of the paddle
+
+# Draw the paddle on the screen
+screen.blit(paddle_surface, (paddle_x, paddle_y))
 
 # while loop to keep the window available until quit
 while True:
@@ -22,3 +35,4 @@ while True:
 
     # display screen until quit
     pygame.display.update()
+
