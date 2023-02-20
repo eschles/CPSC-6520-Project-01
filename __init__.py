48ad127980b4dcd66871d7a88106290a482e8fa0
eschles <67425113+eschles@users.noreply.github.com>
2023-02-20 15:00:10 -0500

    removed init

--- __init__.py
+++
@@ -1,8 +0,0 @@
-#importing .py files into init()
-
-from Stuff import FrameViewer
-from Stuff import GameLoop
-
-sw = FrameViewer.screenColor
-print(sw)
-GameLoop.gameLoop(FrameViewer.screen, FrameViewer.screenColor)
--- main.py
+++ main.py
@@ -1,1 +1,8 @@
 # Your game should have a main script that ties everything together and starts the game loop.
+
+from Stuff import FrameViewer
+from Stuff import GameLoop
+
+sw = FrameViewer.screenColor
+print(sw)
+GameLoop.gameLoop(FrameViewer.screen, FrameViewer.screenColor)
