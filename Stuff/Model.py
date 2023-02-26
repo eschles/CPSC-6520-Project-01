cc617179e47fbf75e1daedaced0e5985cec852df
cmurmu <chandni2104@gmail.com>
2023-02-26 17:04:33 -0500

    Adding Model.py file

---
+++ Stuff/Model.py
@@ -0,0 +1,8 @@
+# Model: Your game should have a model that represents the game's data and the current state of the game.
+# State of the game: Your game should keep track of the state of the game and update it based on 
+# input events and other events.
+
+import pygame
+
+WALL_HIT = 0
+PADDLE_HIT = 0
